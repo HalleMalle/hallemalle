e8b309fe257ff219757fe0e81ceea80ee612b774
dayeon-choi <dayeon2399@naver.com>
2026-05-26 12:06:49 +0900

    style: prettier 세팅

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+
+export default {
+  'semi': false,
+  'singleQuote': true,
+  'jsxSingleQuote': true,
+  'trailingComma': 'es5',
+  'printWidth': 80,
+  'tabWidth': 2,
+  'useTabs': false,
+  'bracketSpacing': true,
+  'bracketSameLine': false,
+  'arrowParens': 'always',
+  'endOfLine': 'lf',
+}
--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 import js from '@eslint/js'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import prettierConfig from 'eslint-config-prettier'
 
 export default [
+  // JS 기본 권장
   js.configs.recommended,
   {
     plugins: {
@@ -10,10 +12,18 @@ export default [
       'react-refresh': reactRefresh,
     },
     rules: {
+      // React Hooks
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
       'no-unused-vars': 'warn',
       'no-console': 'warn',
+      // React Refresh (Vite HMR)
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      // 코드 품질
+      'prefer-const': 'error',
+      'no-var': 'error',
     },
   },
+  // Prettier 충돌 규칙 비활성화
+  prettierConfig,
 ]
