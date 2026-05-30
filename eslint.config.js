717dac58854c8270ec4053e1621c3fa50863b455
iaminsam <iaminsam@sookmyung.ac.kr>
2026-05-30 18:34:11 +0900

    feat: login page 추가
    - GitHub 로그인 페이지 UI와 상태 처리를 추가
    - 로그인 버튼 클릭 시 미설정 안내 에러를 표시
    - Login 페이지 스타일과 Playwright QA 의존성을 추가
    - ESLint Prettier 설정 import 누락을 수정

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import prettierConfig from "eslint-config-prettier";
 import globals from "globals";
 
 export default [
