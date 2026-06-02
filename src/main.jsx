24faba09d97f29364621a47d915ee3a0239ee5ca
dayeon-choi <dayeon2399@naver.com>
2026-06-03 00:25:29 +0900

    refactor: 통일되지 않은 import "" 스타일 통일

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import App from "@/App.jsx";
 
-import '@/styles/base/_global.scss';
+import "@/styles/base/_global.scss";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
